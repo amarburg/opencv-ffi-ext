7511c72ea066ffcf37c96745bae4cbca030cd069
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-07-23 19:38:33 +1200

    Updated SIFT descriptor test.

--- test/features2d/test_sift_descriptor.rb
+++ test/features2d/test_sift_descriptor.rb
@@ -13,32 +13,32 @@ class TestSIFT < Test::Unit::TestCase
 
 
   def test_SIFTDescribe
-  keypoints = [ [100,100], [50,50] ]
-  keypoints = keypoints.map { |kp|
-	  (SIFT::CvSIFTFeature.num_keys-1-kp.length).times {
-	  kp.push 0
-	  }
-	  kp.push ""
+    keypoints = [ [100,100], [50,50] ]
+    keypoints = keypoints.map { |kp|
+      (SIFT::CvSIFTFeature.num_keys-1-kp.length).times {
+        kp.push 0
+      }
+      kp.push ""
 
-kp[6] = kp[0]
-kp[7] = kp[1]
-# If the feature is forced to be at octave 0, interval 0, the scl and feature_data.scl_octv should be SIGMA = 1.6
-kp[2] = kp[11] = 1.6
-		  kp
- }
-p keypoints
+      kp[6] = kp[0]
+      kp[7] = kp[1]
+      # If the feature is forced to be at octave 0, interval 0, the scl and feature_data.scl_octv should be SIGMA = 1.6
+      kp[2] = kp[11] = 1.6
+      kp
+    }
+    p keypoints
 
-kps = SIFT::Results.from_a( keypoints )
-kps.each { |kp| p kp; p kp.feature_data }
+    kps = SIFT::Results.from_a( keypoints )
+    kps.each { |kp| p kp; p kp.feature_data }
 
-params = SIFT::Params.new
-params.recalculateAngles = 1;
-SIFT::detect_describe( @img, params, kps )
+    params = SIFT::Params.new
+    params.recalculateAngles = 1;
+    SIFT::detect_describe( @img, params, kps )
 
-kps.each { |kp|
-p kp.to_a.join(',')
-}
-	end
+    kps.each { |kp|
+      p kp.to_a.join(',')
+    }
+  end
 
 
 	end
