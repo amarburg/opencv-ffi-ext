3be2f529f6f36752dbe50c2fbbab49349e9cb85f
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-07-26 15:28:04 +1200

    Added test case for Chu invariants.

---
+++ test/test_chu_invariants.rb
@@ -0,0 +1,55 @@
+
+require 'test/setup'
+
+require 'opencv-ffi-wrappers'
+require 'opencv-ffi-ext/color_invariance'
+
+class TestChuColorInvariance < Test::Unit::TestCase
+
+  include CVFFI::ColorInvariance
+
+  def setup
+    @img_one = TestSetup::test_image
+  end
+
+  def test_chu_color_invariants
+    img = @img_one.clone
+    assert_not_nil img
+
+    chu_invariants = CVFFI::cvCreateMat( img.height, img.width, :CV_8UC4 )
+    #chu_invariants = CVFFI::cvCreateMat( img.height, img.width, :CV_32FC4 )
+
+    cvGenerateChuQuasiInvariants( img, chu_invariants )
+
+    ## Break out the four channels...
+    
+    hx,hy,sx,sy = chu_invariants.split
+    TestSetup::save_image("chu_invariants_hx", hx )
+    TestSetup::save_image("chu_invariants_hy", hy )
+    TestSetup::save_image("chu_invariants_sx", sx )
+    TestSetup::save_image("chu_invariants_sy", sy )
+  end
+
+  def test_chu_harris
+    img = @img_one.clone
+    assert_not_nil img
+
+    chu_invariants = CVFFI::cvCreateMat( img.height, img.width, :CV_32FC4 )
+    cvGenerateChuQuasiInvariants( img, chu_invariants )
+
+    params = CVFFI::GoodFeaturesParams.new( use_harris: true )
+    corners = chuQuasiInvariantFeatures( chu_invariants, params )
+    puts "Chu quasi invariant found #{corners.length} features"
+
+    feature_img = img.clone
+    corners.each { |corner|
+      CVFFI::cvCircle( feature_img, CVFFI::CvPoint.new( :x => corner.x, :y => corner.y ), 20,
+                                            CVFFI::CvScalar.new( :w=>255, :x=>255, :y=>0, :z=>0 ), -1, 8, 0 )
+    }
+    TestSetup::save_image("chu_invariant_features", feature_img )
+
+  end
+
+
+
+end
